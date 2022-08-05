eae8577a5124f725b3bab86a5774ee454d95e122
soaresgus <gustavosoaresbt@hotmail.com>
2022-08-05 09:49:55 -0300

    Create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { AuthProvider } from './context/AuthProvider';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ProtectedLayout } from './components/ProtectedLayout';
+import { Login } from './components/Login';
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/profile"
+            element={
+              <ProtectedLayout>
+                <h1>Perfil!</h1>
+              </ProtectedLayout>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
 export default App;
