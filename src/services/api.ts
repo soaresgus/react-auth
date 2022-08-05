eb68cd2a22fe0ffbdf1ce3a79589e31ff51446df
soaresgus <gustavosoaresbt@hotmail.com>
2022-08-05 09:48:52 -0300

    Create interceptors on api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,21 @@
 import axios from 'axios';
+import { getUserLocalStorage } from '../context/AuthProvider/util';
 
 export const Api = axios.create({
   baseURL: 'https://reqres.in/api/',
 });
+
+Api.interceptors.request.use(
+  (config) => {
+    const user = getUserLocalStorage();
+
+    if (config.headers) {
+      config.headers.Authorization = user?.token;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
